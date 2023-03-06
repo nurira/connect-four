fa13d86b2bb2364a1f2c07f9d6182a8c9619011f
nrira <ranuri@pm.me>
2023-03-06 14:26:35 -0500

    add score and turn displays to game

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components/macro";
 import Text from "../Text";
 import Logo from "../Logo";
 import Board from "./Board";
+import ScoreDisplay from "./ScoreDisplay";
+import TurnDisplay from "./TurnDisplay";
+import { BREAKPOINTS } from "../GlobalStyles";
 
 export default function Game() {
   return (
@@ -12,31 +15,73 @@ export default function Game() {
           <GameButton>
             <Text size="xs">Menu</Text>
           </GameButton>
-          <Logo />
+          <Logo size={46} />
           <GameButton>
             <Text size="xs">Restart</Text>
           </GameButton>
         </Header>
-        <Board />
+        <PlayerScores>
+          <ScoreDisplay player={1} score={12} />
+          <ScoreDisplay player={2} score={23} />
+        </PlayerScores>
+        <BoardWrapper>
+          <Board />
+          <TurnDisplay player={1} />
+        </BoardWrapper>
       </MaxWidthContainer>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  padding: 20px;
   height: 100%;
+  padding: 48px 20px;
   background: var(--color-primary-light);
+
+  &::before {
+    content: "";
+    width: 100%;
+    height: 236px;
+    background: var(--color-primary);
+    position: absolute;
+    border-radius: 60px 60px 0 0;
+    bottom: 0;
+    left: 0;
+
+    @media ${BREAKPOINTS.desktop} {
+      height: 200px;
+    }
+  }
 `;
 
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const MaxWidthContainer = styled.div`
   max-width: 632px;
   margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  gap: 47px;
+`;
+
+const PlayerScores = styled.div`
+  display: flex;
+  margin: 0 auto;
+  gap: 15px;
+
+  @media ${BREAKPOINTS.desktop} {
+    position: absolute;
+    width: 1034px;
+    left: 0;
+    right: 0;
+    justify-content: space-between;
+  }
 `;
 
 const GameButton = styled.button`
@@ -52,4 +97,8 @@ const GameButton = styled.button`
     color: var(--color-white);
     text-transform: uppercase;
   }
+`;
+
+const BoardWrapper = styled.div`
+  isolation: isolate;
 `;
