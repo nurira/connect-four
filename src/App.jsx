d0a44bba4546eee69026a1608bbdbc7575589da4
nrira <ranuri@pm.me>
2023-03-05 20:51:41 -0500

    extract StartMenu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
 import styled from "styled-components/macro";
 
-import Button from "./Button";
-import Text from "./Text";
+import StartMenu from "./StartMenu";
 
-function App() {
+export default function App() {
   return (
     <Wrapper>
-      <Button type="tertiary">
-        <Text>Play vs. Player</Text>
-      </Button>
-      <Button>
-        <Text>Game Rules</Text>
-      </Button>
+      <StartMenu />
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
-  height: 480px;
-  width: 480px;
-  border-radius: 40px;
-  border: 3px solid var(--color-black);
-  background: var(--color-primary-light);
-
-  filter: var(--drop-shadow-black);
-  margin: 0 auto;
-
-  padding: 70px 40px;
+const Wrapper = styled.main`
+  height: 100%;
 `;
-
-export default App;
---
+++ src/StartMenu.jsx
@@ -0,0 +1,63 @@
+import styled from "styled-components/macro";
+import { BREAKPOINTS } from "./GlobalStyles";
+
+import Logo from "./Logo";
+import Button from "./Button";
+import Text from "./Text";
+
+export default function StartMenu() {
+  return (
+    <Wrapper>
+      <LogoWrapper>
+        <Logo size={60} />
+      </LogoWrapper>
+      <Actions>
+        <Button type="tertiary">
+          <Text>Play vs. Player</Text>
+        </Button>
+        <Button>
+          <Text>Game Rules</Text>
+        </Button>
+      </Actions>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  background: var(--color-primary-light);
+  padding: 57px 17px;
+  width: 100%;
+  height: 100%;
+
+  /* tablet styles */
+  @media ${BREAKPOINTS.tablet} {
+    height: 440px;
+    width: 486px;
+    position: absolute;
+    inset: 0;
+    margin: auto;
+    border: var(--border);
+    border-color: var(--color-black);
+    border-radius: 40px;
+    filter: var(--drop-shadow-black);
+    padding-inline: 37px;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  width: fit-content;
+  margin: 0 auto;
+  margin-top: 143px;
+  margin-bottom: 70px;
+
+  @media ${BREAKPOINTS.tablet} {
+    margin-top: 10px;
+  }
+`;
+
+const Actions = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 24px;
+`;
