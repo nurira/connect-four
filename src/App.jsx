5478d944ae3b27975a189fa94aa0604a08770326
nrira <ranuri@pm.me>
2023-03-05 23:58:46 -0500

    write basic state for rule card display

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import styled from "styled-components/macro";
 
+import RuleCard from "./RuleCard";
 import StartMenu from "./StartMenu";
 
 export default function App() {
-  return (
-    <Wrapper>
-      <StartMenu />
-    </Wrapper>
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
+
+  const handleRules = () => {
+    setIsPlaying(true);
+  };
+
+  return isPlaying ? (
+    <RuleCard />
+  ) : (
+    <StartMenu onPlayGame={handlePlay} onShowRules={handleRules} />
   );
 }
-
-const Wrapper = styled.main`
-  height: 100%;
-`;
