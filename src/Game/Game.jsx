f4fd3c6618eab2f2acc2a329c9979f7dd1821b9f
nrira <ranuri@pm.me>
2023-03-06 11:44:24 -0500

    change board component to use picture elements

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,15 +1,55 @@
 import styled from "styled-components/macro";
 
+import Text from "../Text";
+import Logo from "../Logo";
 import Board from "./Board";
 
 export default function Game() {
   return (
     <Wrapper>
-      <Board />
+      <MaxWidthContainer>
+        <Header>
+          <GameButton>
+            <Text size="xs">Menu</Text>
+          </GameButton>
+          <Logo />
+          <GameButton>
+            <Text size="xs">Restart</Text>
+          </GameButton>
+        </Header>
+        <Board />
+      </MaxWidthContainer>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   padding: 20px;
+  height: 100%;
+  background: var(--color-primary-light);
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const MaxWidthContainer = styled.div`
+  max-width: 632px;
+  margin: 0 auto;
+`;
+
+const GameButton = styled.button`
+  border-radius: 20px;
+  background: var(--color-primary);
+  padding: 10px;
+  border: none;
+
+  width: ${108 / 16}rem;
+  font-family: "Space GroteskVariable";
+
+  span {
+    color: var(--color-white);
+    text-transform: uppercase;
+  }
 `;
