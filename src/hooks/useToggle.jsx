a6a06a0d518374705170b7ae035fe0dfbb3bbdc4
nrira <ranuri@pm.me>
2023-03-06 08:52:39 -0500

    create toggle custom hook

---
+++ src/hooks/useToggle.jsx
@@ -0,0 +1,11 @@
+export default function useToggle(initialState = false) {
+  if (typeof initialState !== "boolean") {
+    console.warn(
+      `useToggle(${initial}) is not being initialized with a boolean value.`
+    );
+  }
+
+  const [toggle, setToggle] = React.useState(initialState);
+
+  return [toggle, () => setToggle(!toggle)];
+}
