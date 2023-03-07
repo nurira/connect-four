0e0313bd29bf437930ab8bae1fbb13cbf284095e
nrira <ranuri@pm.me>
2023-03-06 23:23:03 -0500

    modify to properly display board state

--- src/Game/Board.jsx
+++ src/Game/Board.jsx
@@ -3,7 +3,7 @@ import { BREAKPOINTS } from "../GlobalStyles";
 
 import Piece from "./Piece";
 
-export default function Board() {
+export default function Board({ boardState, onClick, player }) {
   return (
     <Wrapper>
       <Background>
@@ -14,11 +14,18 @@ export default function Board() {
         <img src={`./assets/board-layer-black-small.svg`} />
       </Background>
       <PieceLayer>
-        {Array(42)
-          .fill(null)
-          .map((_, idx) => (
-            <Piece color={idx % 2 === 0 ? "red" : "yellow"} />
-          ))}
+        {boardState.map((column, colIdx) =>
+          column.map((piece, rowIdx) => (
+            <Piece
+              key={rowIdx + colIdx}
+              style={{
+                gridRow: 6 - rowIdx,
+                gridColumn: colIdx + 1,
+              }}
+              color={piece === 1 ? "red" : "yellow"}
+            />
+          ))
+        )}
       </PieceLayer>
       <Foreground>
         <source
@@ -27,6 +34,13 @@ export default function Board() {
         />
         <img src={`./assets/board-layer-white-small.svg`} />
       </Foreground>
+      <ClickLayer>
+        {Array(7)
+          .fill(null)
+          .map((col, idx) => (
+            <ClickColumn player={player} onClick={() => onClick(idx)} />
+          ))}
+      </ClickLayer>
     </Wrapper>
   );
 }
@@ -60,9 +74,52 @@ const PieceLayer = styled.div`
   grid-column-gap: 5.6px;
   grid-row-gap: 0.6px;
 
+  overflow: hidden;
+
   @media ${BREAKPOINTS.tablet} {
     grid-column-gap: 18px;
     grid-row-gap: 13px;
     padding: 17px;
+  }
+`;
+
+const ClickLayer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  padding: 5px;
+  padding-bottom: 32px;
+  gap: 1px;
+
+  @media ${BREAKPOINTS.tablet} {
+    padding: 8px;
+  }
+`;
+
+const ClickColumn = styled.div`
+  position: relative;
+  cursor: pointer;
+  height: 100%;
+  flex: 1;
+  /* background: deeppink; */
+
+  @media ${BREAKPOINTS.desktop} {
+    &:hover::before {
+      content: "";
+      background: no-repeat
+        ${({ player }) =>
+          player === 1
+            ? `url("./assets/marker-red.svg")`
+            : `url("./assets/marker-yellow.svg")`};
+      height: 38px;
+      width: 38px;
+      position: absolute;
+      top: -7px;
+      left: 50%;
+      transform: translate(-50%, -100%);
+    }
   }
 `;
