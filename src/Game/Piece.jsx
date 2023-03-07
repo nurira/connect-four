1bf5aca5880df4e24dbb2f897e16918e35e0cbdb
nrira <ranuri@pm.me>
2023-03-06 23:23:43 -0500

    add animation for board piece drops

--- src/Game/Piece.jsx
+++ src/Game/Piece.jsx
@@ -1,14 +1,34 @@
-import styled from "styled-components/macro";
+import styled, { keyframes } from "styled-components/macro";
 import { BREAKPOINTS } from "../GlobalStyles";
 
-export default function Piece({ color = "red" }) {
+export default function Piece({ color = "red", ...delegated }) {
   return (
-    <picture>
+    <Wrapper {...delegated}>
       <source
         media={BREAKPOINTS.tablet}
         srcSet={`./assets/counter-${color}-large.svg`}
       />
       <img src={`./assets/counter-${color}-small.svg`} />
-    </picture>
+    </Wrapper>
   );
 }
+
+const drop = keyframes`
+  0% {
+    transform: translateY(-450px);
+  } 
+  90% {
+    transform: translateY(0);
+  }
+  95% {
+    transform: translateY(-10px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+const Wrapper = styled.picture`
+  position: relative;
+  animation: ${drop} 0.2s ease-out both;
+`;
